cef95fd97b0d1bea54c754ce3f663512c16a4ffd
Yuan <54chenyuan@gmail.com>
2018-11-27 15:22:31 +0100

    Add JetTool Box for 2016

--- test/haa4b/results_2018_08_17/FARM/inputs/_MC13TeV_Zh_amass20.cmd
+++
@@ -1,5 +0,0 @@
-####################################
-#        LaunchOnFarm Script       #
-####################################
-
-crab submit -c /afs/cern.ch/user/y/yuanc/Analysis/CMSSW_8_0_26_patch1/src/UserCode/bsmhiggs_fwk/test/haa4b/results_2018_08_17/FARM/inputs/crabConfig_0000__MC13TeV_Zh_amass20_cfg.py
