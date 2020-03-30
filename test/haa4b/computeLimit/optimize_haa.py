d1cbed0ac9a2391fc83dc9c5243326a065a5873b
yuanc <yuan.chen@cern.ch>
2020-03-30 12:55:18 +0200

    clean optimize_haa.py

--- test/haa4b/computeLimit/optimize_haa.py
+++ test/haa4b/computeLimit/optimize_haa.py
@@ -45,8 +45,8 @@ inUrl_wh='$CMSSW_BASE/src/UserCode/bsmhiggs_fwk/test/haa4b/plotter_2016_WH_forLi
 
 BESTDISCOVERYOPTIM=True #Set to True for best discovery optimization, Set to False for best limit optimization
 ASYMTOTICLIMIT=True #Set to True to compute asymptotic limits (faster) instead of toy based hybrid-new limits
-#BINS = ["3b","4b","3b,4b"] # list individual analysis bins to consider as well as combined bins (separated with a coma but without space)
-BINS = ["3b,4b"] # list individual analysis bins to consider as well as combined bins (separated with a coma but without space)
+BINS = ["3b","4b","3b,4b"] # list individual analysis bins to consider as well as combined bins (separated with a coma but without space)
+#BINS = ["3b,4b"] # list individual analysis bins to consider as well as combined bins (separated with a coma but without space)
 
 MASS = [12, 15, 20, 25, 30, 40, 50, 60]
 SUBMASS = [12, 15, 20, 25, 30, 40, 50, 60]
@@ -62,7 +62,6 @@ for model in MODELS:
             suffix = "" 
             signalSuffixVec += [ suffix ]
             OUTName         += ["SB13TeV_SM_Wh"]
-#            OUTName         += ["SB13TeV_SM_Wh"]
             LandSArgOptions += [" --histo " + shape + "  --systpostfix _13TeV --shape "]
             BIN             += [bin]
             MODEL           += [model]
