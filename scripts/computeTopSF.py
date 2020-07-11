3b4cbb280d291ee54676ee6ac48f848b32665993
yuanc <yuan.chen@cern.ch>
2020-07-11 17:54:14 +0200

    latest updates on DY Z Pt and Top weights

--- scripts/computeTopSF.py
+++ scripts/computeTopSF.py
@@ -10,16 +10,17 @@ from array import array
 import math
 from ROOT import SetOwnership
 
+import CMS_lumi
+CMS_lumi.writeExtraText = 1
+CMS_lumi.extraText = "Preliminary"
+iPos = 11
+iPeriod = 0
 
 bkgds = ["otherbkg", "ttbarbba", "ttbarcba", "ttbarlig", "zll", "wlnu", "ddqcd", "qcd"]
 #startbin = 9 
 
 low=-1
 high=-1
-a_3b = 0
-b_3b = 0
-a_4b = 0
-b_4b = 0
 
 def drawHist(hist, name):
 
@@ -35,31 +36,21 @@ def drawHist(hist, name):
     return c
 
 def applyWeights(hist, name):
-    global a_3b
-    global a_4b
-    global b_3b
-    global b_4b
     thred_low = 0
     thred_high = 500
-    if vh_tag=="zh":
-        thred_low = int(low)
-	thred_high = int(high)
+#    if vh_tag=="zh":
+#        thred_low = int(low)
+#	thred_high = int(high)
     for i in range(1, hist.GetNbinsX()+1):
         sf=1.0
 	ht = hist.GetBinCenter(i)
 	if ht>=thred_low and ht<=thred_high:
 	    if '3b' in name:
-		#sf = math.exp(0.04182-0.00095*ht) # Wh 2016
-		#sf = math.exp(-0.07758-0.00175*ht) # Wh 2017
-		sf = math.exp(0.07997+0.00001*ht) # Wh 2018
+		sf = math.exp(0.06117-0.00134*ht) # Wh 2016
 		
-		#sf = math.exp(a_3b+b_3b*ht)
 	    elif '4b' in name:
-		sf = math.exp(0.00503-0.00036*ht) # Wh 2018
-		#sf = math.exp(-0.07055-0.00171*ht) # Wh 2017
-		#sf = math.exp(0.02629-0.00098*ht) # Wh 2016
+		sf = math.exp(0.05567-0.00182*ht) # Wh 2016
 		
-		#sf = math.exp(a_4b+b_4b*ht)
 	hist.SetBinContent(i, hist.GetBinContent(i)*sf)
 
         
@@ -88,33 +79,79 @@ def weightedAverage(ratio, hNLO, threshold, end=800):
 	ratio.SetBinError(i,error)
     return ratio
 
-def ratioOnly(ratio_h, name):
-    c = r.TCanvas(name,name,800,600)
-    ratio = ratio_h.DrawCopy(name)
-    ratio.SetTitle(name.replace("_"," "))
-    pad = r.TPad("pad","pad", 0, 0, 1, 1)
-    pad.SetBottomMargin(0.1)
-    pad.Draw()
-    pad.cd()
-    fitf = r.TF1(name+"_f", "exp([0]+[1]*x)", 0, 500)
-    ratio.Fit(name+"_f", "R")
-    ratio.GetYaxis().SetRangeUser(0.4, 1.5)
-    ratio.GetXaxis().SetTitleOffset(1.0)
-    ratio.Draw("E0")
+def ratioFit(ratio_h, name, outdir):
+    c = r.TCanvas(name,name,800,800)
+    ratio = ratio_h.Clone(name+"_95%CL")
+    ratio.SetMaximum(min(2, max(1.3, 1.3*ratio.GetMaximum())))
+    ratio.SetMinimum(max(0, 0.8*ratio.GetMinimum()))
+#    ratio.GetXaxis().SetRangeUser(0,300)
+    ratio.SetTitle(" ")
+    ratio.SetMarkerStyle(20)
+    ratio.SetMarkerSize(1.1)
+    ratio.GetYaxis().SetTitle("Correction")
+    ratio.GetYaxis().SetTitleSize(35)
+    ratio.GetYaxis().SetTitleFont(43)
+    ratio.GetYaxis().SetTitleOffset(1.1)
+    ratio.GetYaxis().SetLabelFont(43)
+    ratio.GetYaxis().SetLabelSize(30)
+    ratio.GetXaxis().SetTitleOffset(1.5)
+    ratio.GetXaxis().SetTitleSize(25)
+    ratio.GetXaxis().SetTitleFont(43)
+    ratio.GetXaxis().SetLabelFont(43)
+    ratio.GetXaxis().SetLabelSize(30)
+
+    thred = int(low)
+    fitf = r.TF1(name+"_f_95%CL", "expo", thred, int(high))
+#    fitf = r.TF1(name+"_f_95%CL", "expo", 0, 800)
+    fitf.SetLineColor(r.kBlack)
+    fitf.SetLineWidth(2)
+    res = ratio.Fit(fitf, "R S")
+    fitf.SetRange(0, 800)
+
+    values = res.GetConfidenceIntervals(0.95, False)
+    print("values:")
+    print(len(values))
+    interval = r.TGraphErrors(len(values))
+    for i in range(len(values)):
+	if i == 0:
+	    xPos = thred + ratio.GetXaxis().GetBinLowEdge(i+1)
+#	    interval.SetPoint(i, ratio.GetXaxis().GetBinLowEdge(i+1), fitf.Eval( ratio.GetXaxis().GetBinLowEdge(i+1) ))
+	elif i == (len(values)-1):
+	    xPos = thred + ratio.GetXaxis().GetBinLowEdge(i+2)
+#	    interval.SetPoint(i, ratio.GetXaxis().GetBinLowEdge(i+2), fitf.Eval( ratio.GetXaxis().GetBinLowEdge(i+2) ))
+	    #interval.SetPoint(i, 300, fitf.Eval( 300 ))
+        else: 
+	    xPos = thred + ratio.GetXaxis().GetBinCenter(i+1)
+#	    interval.SetPoint(i, ratio.GetXaxis().GetBinCenter(i+1), fitf.Eval( ratio.GetXaxis().GetBinCenter(i+1) ))
+	interval.SetPoint(i, xPos, fitf.Eval(xPos))
+	interval.SetPointError(i, 0, values[i] )
+    #interval.SetFillColor(r.kRed-9)
+    interval.SetFillColor(r.kOrange)
+    #interval.SetFillColor(r.kViolet-9)
+    interval.Draw("3")
+    ratio.Draw("same")
+    fitf.SetRange(0, 800)
+    fitf.Draw("same")
+
+    leg = r.TLegend(0.6,0.74,0.89,0.89)
+    leg.AddEntry(ratio, "Measured")
+    text = "#splitline{Exponential Fit(95% CL)}{#chi^{2} = " + "{:.2f}".format(fitf.GetChisquare())  + " }"
+    #leg.AddEntry(interval, "#splitline{Exponential Fit(95% CL)}{#chi^{2} = }", "p")
+    leg.AddEntry(interval, text)
+    leg.SetBorderSize(0)
+    leg.Draw("same")
+
+    CMS_lumi.CMS_lumi(c, iPeriod, iPos)
+    c.cd()
+    c.Update()
+    
+    c.SaveAs(os.path.join(outdir,"topPtSF_"+name[-2:]+"_Fit.pdf"))
+#    raw_input("Press [ENTER] to exit...")
     return c
 
 def ratioPlot(hLO,hNLO,ratio_h,name):
     global CC
-    global a_3b
-    global a_4b
-    global b_3b
-    global b_4b
 
-    thred = 500
-    if '3b' in name: thred = 400
-    elif '4b' in name: thred = 400
-#    ratio_h = weightedAverage(ratio_h,hNLO,thred)
-    
     # Define the Canvas
     c = r.TCanvas(name,name,800,800)
     
@@ -176,6 +213,7 @@ def ratioPlot(hLO,hNLO,ratio_h,name):
     # Define the ratio plot
     ratio = ratio_h.DrawCopy("ehist_"+name)
 #    ratio.GetXaxis().SetRangeUser(0,300)
+#    ratio.GetYaxis().SetRangeUser(0,2)
     ratio.GetYaxis().SetRangeUser(0.4,1.6)
 #    ratio.GetYaxis().SetRangeUser(0.9*ratio.GetMinimum(),2)
 #    ratio = hNLO.Clone(name+"_clone")
@@ -202,31 +240,20 @@ def ratioPlot(hLO,hNLO,ratio_h,name):
     ratio.GetXaxis().SetLabelFont(43)
     ratio.GetXaxis().SetLabelSize(20)
 
-#    c1 = ratioOnly(ratio, name+"_ratio")
     pad2.cd()
     fitf = r.TF1(name+"_f", "exp([0]+[1]*x)", int(low), int(high))
-    ratio.Draw("E0")
     if CC=="False":
       print("name of function: {}".format(name+"_f"))
       ratio.Fit(name+"_f", "R")
-      func = ratio.GetFunction(name+"_f")
-      print("Chi square of expo is: {}".format(func.GetChisquare()))
       if showfull=="True":
+	func = ratio.GetFunction(name+"_f")
         func.SetRange(0, 500)
-      func.Draw("same")
-      a = func.GetParameter(0)
-      b = func.GetParameter(1)
-      if "3b" in name:
-        a_3b = a
-	b_3b = b
-      elif "4b" in name:
-        a_4b = a
-	b_4b = b
-
-    r.gPad.Update()
-    line = r.TLine(r.gPad.GetUxmin(), 1, r.gPad.GetUxmax(), 1)
-    line.Draw("same")
-    SetOwnership( line, 0 )
+    
+    ratio.Draw("E0")
+#    r.gPad.Update()
+#    line = r.TLine(r.gPad.GetUxmin(), 1, r.gPad.GetUxmax(), 1)
+#    line.Draw("same")
+#    SetOwnership( line, 0 )
    
 #    return c,func
     return c
@@ -237,18 +264,19 @@ def getHists(channels, inf):
 	if not data:
 	    print("Error, cannot find data in the channel: {}".format(ch))
 	    exit(-1)
-	dataSub = data.Clone("dataSub_h")
+	dataSub = data.Clone("dataSub_h_"+ch[-2:])
 	for bkgd in bkgds:
 	    if("ttbar" in bkgd): # add in ttbarMC
+	    #if("ttbarlig" in bkgd): # add in ttbarMC
 		h_mc = inf.Get(ch+"/"+bkgd)
 		if h_mc:
 		    if("ttbarMC" in vars() or "ttbarMC" in globals()): ttbarMC.Add(h_mc)
-		    else: ttbarMC = h_mc.Clone("ttbarMC_h")
-	    else:   # add in onttbarMC
+		    else: ttbarMC = h_mc.Clone("ttbarMC_h_"+ch[-2:])
+	    else:   # add in nonttbarMC
 		h_mc = inf.Get(ch+"/"+bkgd)
 		if h_mc:
 		    if("nonttbarMC" in vars() or "nonttbarMC" in globals()): nonttbarMC.Add(h_mc)
-		    else: nonttbarMC = h_mc.Clone("nonttbarMC_h")
+		    else: nonttbarMC = h_mc.Clone("nonttbarMC_h_"+ch[-2:])
 	if not ("ttbarMC" in vars() or "ttbarMC" in globals()):
 	    print("Error, cannot find ttbar process in the channel: {}".format(ch))
 	    exit(-1)
@@ -263,26 +291,28 @@ def getHists(channels, inf):
     return dataSub_ret, ttbarMC_ret
 
 def computeTopPtSF(channels, inf, outdir, outf):
+    global CC
     postfix=""
     if CC=="True": postfix="_CC"
     dataSub_h, ttbarMC_h = getHists(channels, inf)
-    if CC=="True":
-        applyWeights(ttbarMC_h, "weights"+channels[0][-2:])
+    #if CC=="True":
+    #    applyWeights(ttbarMC_h, "weights"+channels[0][-2:])
     weights = dataSub_h.Clone("weights"+channels[0][-2:]+postfix)
     weights.Divide(ttbarMC_h)
-#    c,f = ratioPlot(data, bkgd, weights, "topPtSF_"+channels[0][-2:])
-    c = ratioPlot(dataSub_h, ttbarMC_h, weights, "topPtSF_"+channels[0][-2:]+postfix)
-    c.SaveAs(os.path.join(outdir,"topPtSF_"+channels[0][-2:]+".pdf")+postfix)
-    outf.cd()
+#    c = ratioPlot(dataSub_h, ttbarMC_h, weights, "topPtSF_"+channels[0][-2:]+postfix)
+#    c.Write()
+    
+    c = ratioFit(weights, "topPtSF_"+channels[0][-2:], outdir)
+#    c.SaveAs(os.path.join(outdir,"topPtSF_"+channels[0][-2:]+"_Fit.pdf"))
     c.Write()
-    dataSub_h.Write()
-    ttbarMC_h.Write()
-    weights.Write()
+    #c.SaveAs(os.path.join(outdir,"topPtSF_"+channels[0][-2:]+".pdf")+postfix)
+    #outf.cd()
+    #c.Write()
 
 
 try:
      # retrive command line options
-    shortopts  = "d:o:l:f:u:t:c:?" #RJ
+    shortopts  = "d:o:l:a:f:u:t:c:?" #RJ
     opts, args = getopt.getopt( sys.argv[1:], shortopts )
 except getopt.GetoptError:
     # print help information and exit:
@@ -304,10 +334,10 @@ showfull="True"
 for o,a in opts:
     if o in('-d'): inputdir = a
     elif o in('-o'): outdir = a
-    elif o in('-t'): vh_tag = a
+    elif o in('-c'): vh_tag = a
     elif o in('-l'): low = a
     elif o in('-u'): high = a
-    elif o in('-c'): CC = a
+    elif o in('-a'): CC = a
     elif o in('-f'): showfull = a
 
 print("vh_tag = {}".format(vh_tag))
@@ -330,22 +360,17 @@ if not inf.IsOpen():
     print("The file cannot be opened, please check: " + fpath)
     exit(-1)
 
+
 outname = "topSF.root"
 if CC=="True": outname = "topSF_CC.root"
 outf = r.TFile(os.path.join(outdir,outname), 'RECREATE')
-#outf_CC = r.TFile(os.path.join(outdir,"topSF_CC.root"), 'RECREATE')
 ######## 3b bin ##########
 computeTopPtSF(hists_3b, inf, outdir, outf)
 
 ######## 4b bin ##########
 computeTopPtSF(hists_4b, inf, outdir, outf)
 
-#CC="True"
-#computeTopPtSF(hists_3b, inf, outdir, outf)
-#computeTopPtSF(hists_4b, inf, outdir, outf)
-
 inf.Close()
 outf.Close()
-#outf_CC.Close()
 
-raw_input("Press [ENTER] to exit...")
+#raw_input("Press [ENTER] to exit...")
