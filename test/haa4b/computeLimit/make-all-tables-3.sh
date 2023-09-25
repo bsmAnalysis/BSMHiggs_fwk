5a1f9656e496d7d9634db8653af050853f7690a3
georgia <georgia.karapostoli@cern.ch>
2023-09-25 13:19:39 +0200

    scripts for merged results

---
+++ test/haa4b/computeLimit/make-all-tables-3.sh
@@ -0,0 +1,10 @@
+
+python yieldTableBDTcut3_full.py cards_SB13TeV_SM_Wh_all_noSoftb_Combined/../
+
+mv yield-table-in-bdt-bins-A_SR_3b.tex yield-table-in-bdt-bins-wh-A_SR_3b.tex
+mv yield-table-in-bdt-bins-A_SR_4b.tex yield-table-in-bdt-bins-wh-A_SR_4b.tex
+
+python yieldTableBDTcut3_full.py cards_SB13TeV_SM_Zh_all_noSoftb_Combined/../
+
+mv yield-table-in-bdt-bins-A_SR_3b.tex yield-table-in-bdt-bins-zh-A_SR_3b.tex   
+mv yield-table-in-bdt-bins-A_SR_4b.tex yield-table-in-bdt-bins-zh-A_SR_4b.tex  
---
+++ test/haa4b/computeLimit/make-the-postfit-plots-3.sh
@@ -0,0 +1,8 @@
+python postfitShapes3.py zh ./
+python postfitShapes3.py zh ./ --do_liny 
+python postfitShapes3.py zh ./ --do_linzoom
+
+python postfitShapes3.py wh ./
+python postfitShapes3.py wh ./ --do_liny
+python postfitShapes3.py wh ./ --do_linzoom
+
